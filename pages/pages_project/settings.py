f89657f0ab7ac5d878bcf3aa6e58592ba1fd8288
Daria Butakhina <darinabird46722@gmail.com>
2019-11-23 13:06:36 +0300

    host on heroku

--- pages/pages_project/settings.py
+++ pages/pages_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'jud@^suf8m^puxzmo-is)-3w+)(a0009d@ir-nh4h%7ja9y+^l'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
